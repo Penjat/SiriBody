ff9cfecf2f06b8bf15cc0eff2dd5bca1593f8161
Spencer <37875712+Penjat@users.noreply.github.com>
2022-03-05 19:31:35 -0800

    Added direction.

--- BluetoothConnect/ContentView.swift
+++ BluetoothConnect/ContentView.swift
@@ -11,8 +11,11 @@ struct ContentView: View {
                         DragGesture(minimumDistance: 0)
                             .onChanged { value in
                                 let radius = (geometry.size.width/2)
-                                let motor1Speed = max(-100,min(100,(value.location.y - radius)*100/radius))
-                                let motor2Speed = max(-100,min(100,(value.location.y - radius)*100/radius))
+                                let forwardBackward = max(-100,min(100,(value.location.y - radius)*100/radius))
+                                let leftRight = max(-100,min(100,(value.location.x - radius)*100/radius))
+                                
+                                let motor1Speed = max(-100,min(100,(forwardBackward + leftRight)))
+                                let motor2Speed = max(-100,min(100,(forwardBackward - leftRight)))
                                 print("Touch down \(motor1Speed) \(motor2Speed)")
                                 
                                 viewModel.motor1Speed = Int(motor1Speed < 0 ? abs(motor1Speed) : motor1Speed + 100)
