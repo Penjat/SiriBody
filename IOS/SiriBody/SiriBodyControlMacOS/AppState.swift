ec5c212c381afc1f0b64dde3d2627bb2260e547c
penjat <ssymington@lululemon.com>
2024-10-21 14:28:19 -0700

    Created virtual robit brain.

--- IOS/SiriBody/SiriBodyControlMacOS/AppState.swift
+++ IOS/SiriBody/SiriBodyControlMacOS/AppState.swift
@@ -2,7 +2,9 @@ import Foundation
 import Combine
 
 class AppState: ObservableObject {
-    
+
+    let virtualRobitBrain: RobitBrain
+
     // Service
     let centralService = CentralService(serviceID: TransferService.phoneServiceUUID, charID: TransferService.phoneCharUUID)
     //    let robitPositionService: RobitPositionService
@@ -12,36 +14,48 @@ class AppState: ObservableObject {
 
     init() {
 
+        // Just return 0 for now
+        var controlLogic:  (RobitState?, Command?) -> MotorOutput = { state, command in
+            return MotorOutput.zero
+        }
+
+        // just return origional command for now
+        var objectiveLogic:  (RobitState?, Command?) -> Command? = { state, command in
+            return command
+        }
+
+        self.virtualRobitBrain = RobitBrain(
+            controlLogic: controlLogic,
+            objectiveLogic: objectiveLogic)
+
         setUpSubscriptions()
     }
 
     private func setUpSubscriptions() {
 
-        // Assigns
-
-
         // Connect to Phone
-//        centralService.centralState.sink { [weak self] state in
-//            switch state {
-//            case .unknown:
-//                print("Unkown")
-//            case .resetting:
-//                print("resetting")
-//            case .unsupported:
-//                print("unsupported")
-//            case .unauthorized:
-//                print("unauthorized")
-//            case .poweredOff:
-//                print("poweredOff")
-//            case .poweredOn:
-//                print("poweredOn")
-//                self?.centralService.retrievePeripheral()
-//            @unknown default:
-//                print("unkown")
-//            }
-//        }.store(in: &bag)
+        centralService.centralState.sink { [weak self] state in
+            switch state {
+            case .unknown:
+                print("Unkown")
+            case .resetting:
+                print("resetting")
+            case .unsupported:
+                print("unsupported")
+            case .unauthorized:
+                print("unauthorized")
+            case .poweredOff:
+                print("poweredOff")
+            case .poweredOn:
+                print("poweredOn")
+                self?.centralService.retrievePeripheral()
+            @unknown default:
+                print("unkown")
+            }
+        }.store(in: &bag)
 
         // Phone Input to Real RobitState
+        // TODO: Make this work
 //        centralService
 //            .inputSubject
 //            .throttle(for: .seconds(0.1), scheduler: RunLoop.main, latest: true)
